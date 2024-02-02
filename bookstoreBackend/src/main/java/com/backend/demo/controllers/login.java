2ae65dcd6e01cdd17f41eaab75898a5835e7163d
Yuci Wang <yc457137@dal.ca>
2024-02-02 14:41:31 -0400

    added backend

---
+++ bookstoreBackend/src/main/java/com/backend/demo/controllers/login.java
@@ -0,0 +1,41 @@
+package com.backend.demo.controllers;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+@RestController
+public class login {
+    @PostMapping("/calculate")
+    public Map<String, Object> calculate(@RequestBody Map<String,Object> jsonData)  {
+        Map<String,Object> ans = new HashMap<>();
+        if(jsonData.get("file")==null){
+            ans.put("file",null);
+            ans.put("error","Invalid JSON input.");
+            return ans;
+        }
+        String fileName=jsonData.get("file").toString();
+        String item=jsonData.get("product").toString();
+        File file=new File("/tmp/data/"+fileName);
+        if(!file.exists()){
+            ans.put("file",fileName);
+            ans.put("error","File not found.");
+            return ans;
+        }
+        //source:https://www.baeldung.com/rest-template
+        /*
+         * In the original code at 5.1, it uses a httpEntity to wrap the foo object to send to another
+         * controller. In my code I substituted  the foo with the map data type to pass this to another
+         * container.
+         * */
+        RestTemplate restTemplate = new RestTemplate();
+        HttpEntity<Map<String, Object>> request = new HttpEntity<>(jsonData);
+        ans = restTemplate.postForObject("http://container2:7000/process", request, Map.class);
+        return ans;
+    }
+}
---
+++ bookstoreBackend/src/test/java/com/backend/demo/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
