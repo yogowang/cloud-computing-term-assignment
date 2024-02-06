0de0eaced2b749bc3014ca17529fa9b24139b9a5
Yuci Wang <yc457137@dal.ca>
2024-02-05 22:10:47 -0400

    make changes to login

--- bookStoreUI/views/UserLogin.js
+++ bookStoreUI/views/UserLogin.js
@@ -4,6 +4,7 @@ import {StatusBar} from 'expo-status-bar';
 import axios from "axios";
 import {StackActions as navigation} from "@react-navigation/routers/src";
 //source:https://code.tutsplus.com/common-react-native-app-layouts-login-page--cms-27639t
+//source:https://bobbyhadz.com/blog/react-axios-network-error-stack-trace
 export default function Login(){
     const [userName,setUserName]=useState('');
     const [password,setPassword]=useState('');
@@ -13,7 +14,7 @@ export default function Login(){
             let loginIfo={
                 userName,password
             }
-            axios.post("localhost:8080/login",loginIfo)
+            axios.post("https://100.64.57.28:8080/login",loginIfo)
                 .then((response)=>{
                     if(response.data!=="error"){
                         navigation.replace("Index");
@@ -21,12 +22,16 @@ export default function Login(){
                     else{
                         setException((ex)=>{
                             return {
-                                ...ex,
+                                ex,
                                 authException: "The account doesn't exist!!"
                             };
                         });
                     }
-                });
+                }).catch((ex)=>{
+                    setException("error when login");
+                    console.error(ex);
+                }
+            );
         }
     }
     return(
@@ -47,7 +52,7 @@ export default function Login(){
                 onChangeText={(password)=>setPassword(password)}
                 />
             </View>
-            <TouchableOpacity style={styles.loginBtn}>
+            <TouchableOpacity style={styles.loginBtn} onPress={validateLogin}>
             <Text style={styles.loginText}>LOGIN</Text>
             </TouchableOpacity>
         </View>
--- bookstoreBackend/src/main/java/com/backend/demo/controllers/login.java
+++ bookstoreBackend/src/main/java/com/backend/demo/controllers/login.java
@@ -1,6 +1,8 @@
 package com.backend.demo.controllers;
 
+import com.backend.demo.entity.User;
 import org.springframework.http.HttpEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,33 +11,17 @@ import org.springframework.web.client.RestTemplate;
 import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
+
 @RestController
 public class login {
     @PostMapping("/login")
-    public Map<String, Object> calculate(@RequestBody Map<String,Object> jsonData)  {
-        Map<String,Object> ans = new HashMap<>();
-        if(jsonData.get("file")==null){
-            ans.put("file",null);
-            ans.put("error","Invalid JSON input.");
-            return ans;
+    @CrossOrigin(origins = "*")
+    public String checkLoginInfo(@RequestBody User user)  {
+        String userName=user.getUserName();
+        if(!Objects.equals(userName, "abc")){
+            return "error";
         }
-        String fileName=jsonData.get("file").toString();
-        String item=jsonData.get("product").toString();
-        File file=new File("/tmp/data/"+fileName);
-        if(!file.exists()){
-            ans.put("file",fileName);
-            ans.put("error","File not found.");
-            return ans;
-        }
-        //source:https://www.baeldung.com/rest-template
-        /*
-         * In the original code at 5.1, it uses a httpEntity to wrap the foo object to send to another
-         * controller. In my code I substituted  the foo with the map data type to pass this to another
-         * container.
-         * */
-        RestTemplate restTemplate = new RestTemplate();
-        HttpEntity<Map<String, Object>> request = new HttpEntity<>(jsonData);
-        ans = restTemplate.postForObject("http://container2:7000/process", request, Map.class);
-        return ans;
+        return userName;
     }
 }
